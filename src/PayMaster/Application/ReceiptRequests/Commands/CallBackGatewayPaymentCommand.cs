0c3b080f7d0405773080d5cafb39161b431230b5
Iman Mohammadi <mohammadi4net@gmail.com>
2025-05-22 21:52:19 +0330

    callback result fields

--- src/PayMaster/Application/ReceiptRequests/Commands/CallBackGatewayPaymentCommand.cs
+++ src/PayMaster/Application/ReceiptRequests/Commands/CallBackGatewayPaymentCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Honamic.Framework.Commands;
 using Honamic.Framework.Applications.Authorizes;
 using Honamic.Framework.Applications.Results;
+using Honamic.PayMaster.Enums;
 
 namespace Honamic.PayMaster.Application.ReceiptRequests.Commands;
 
@@ -29,5 +30,28 @@ public class CallBackGatewayPaymentCommand : ICommand<Result<CallBackGatewayPaym
 
 public class CallBackGatewayPaymentCommandResult
 {
+    public CallBackGatewayPaymentCommandResult()
+    {
+        GatewayPayments = [];
+    }
 
+    public ReceiptRequestStatus Status { get; set; }
+    public string ReceiptRequestId { get; set; }
+    public string? IssuerReference { get; set; }
+    public string? PartyReference { get; set; }
+    public decimal Amount { get; set; }
+    public string Currency { get; set; }
+    public string? AdditionalData { get; set; }
+    public object? Issuer { get; set; }
+
+    public List<CallBackGatewayPaymentGatewayPaymentsCommandResult> GatewayPayments { get; set; }
+}
+public class CallBackGatewayPaymentGatewayPaymentsCommandResult
+{
+    public string Id { get; internal set; }
+    public PaymentGatewayStatus Status { get; internal set; }
+    public decimal Amount { get; internal set; }
+    public string Currency { get; internal set; }
+    public PaymentGatewayFailedReason FailedReason { get; internal set; }
+    public string? StatusDescription { get; internal set; }
 }
