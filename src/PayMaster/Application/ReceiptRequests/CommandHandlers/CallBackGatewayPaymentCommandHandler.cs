11ea5db5977aa999d398fa4811ab80308de32f2d
Iman Mohammadi <mohammadi4net@gmail.com>
2025-05-27 22:11:10 +0330

    Refactor Callback processing with new domain service

--- src/PayMaster/Application/ReceiptRequests/CommandHandlers/CallBackGatewayPaymentCommandHandler.cs
+++ src/PayMaster/Application/ReceiptRequests/CommandHandlers/CallBackGatewayPaymentCommandHandler.cs
@@ -1,166 +1,58 @@
 ﻿using Honamic.Framework.Applications.Results;
 using Honamic.Framework.Commands;
-using Honamic.Framework.Domain;
 using Honamic.PayMaster.Application.ReceiptRequests.Commands;
-using Honamic.PayMaster.Domains.PaymentGatewayProviders;
-using Honamic.PayMaster.Domains.ReceiptRequests;
-using Honamic.PayMaster.Domains.ReceiptRequests.Exceptions;
 using Honamic.PayMaster.Domains.ReceiptRequests.Services;
-using Honamic.PayMaster.PaymentProviders;
-using Honamic.PayMaster.PaymentProviders.Models;
-using IdGen;
 using System.Globalization;
 
 namespace Honamic.PayMaster.Application.ReceiptRequests.CommandHandlers;
 internal class CallBackGatewayPaymentCommandHandler : ICommandHandler<CallBackGatewayPaymentCommand, Result<CallBackGatewayPaymentCommandResult>>
 {
-    private readonly IReceiptRequestRepository _receiptRequestRepository;
-    private readonly ICreatePaymentDomainService _createPaymentDomainService;
-    private readonly IPaymentGatewayProviderRepository _repository;
-    private readonly IPaymentGatewayProviderFactory _factory;
-    private readonly IClock _clock;
+    private readonly ICallbackGatewayPaymentDomainService _callBackGatewayPaymentDomainService;
 
     public CallBackGatewayPaymentCommandHandler(
-        IReceiptRequestRepository receiptRequestRepository,
-        ICreatePaymentDomainService createPaymentDomainService,
-        IPaymentGatewayProviderFactory factory,
-        IPaymentGatewayProviderRepository repository,
-        IClock clock)
+        ICallbackGatewayPaymentDomainService callBackGatewayPaymentDomainService)
     {
-        _receiptRequestRepository = receiptRequestRepository;
-        _createPaymentDomainService = createPaymentDomainService;
-        _factory = factory;
-        _repository = repository;
-        _clock = clock;
+        _callBackGatewayPaymentDomainService = callBackGatewayPaymentDomainService;
     }
 
-    public async Task<Result<CallBackGatewayPaymentCommandResult>> HandleAsync(CallBackGatewayPaymentCommand command, CancellationToken cancellationToken)
+    public async Task<Result<CallBackGatewayPaymentCommandResult>> HandleAsync(
+        CallBackGatewayPaymentCommand command,
+        CancellationToken cancellationToken)
     {
-        ReceiptRequest? receiptRequest = null;
-        ReceiptRequestGatewayPayment? gatewayPayment = null;
-        IPaymentGatewayProvider? paymentGatewayProvider = null;
-        ExtractCallBackDataResult ExtractCallBackDataResult;
         var gatewayPaymentId = command.GetGatewayPaymentIdAsLong();
         var gatewayProviderId = command.GetGatewayProviderIdAsLong();
 
-        if (gatewayPaymentId.HasValue)
-        {
-            receiptRequest = await _receiptRequestRepository
-                  .GetByGatewayPaymentIDAsync(gatewayPaymentId.Value);
-
-            if (receiptRequest is null)
-            {
-                throw new InvalidPaymentException();
-            }
-
-            gatewayPayment = receiptRequest.GetGatewayPayment(gatewayPaymentId.Value);
-
-            if (gatewayPayment is null)
-            {
-                throw new InvalidPaymentException();
-            }
-
-            if (gatewayPayment.GatewayProviderId != gatewayProviderId)
-            {
-                throw new InvalidPaymentException();
-            }
-
-            var gatewayProvider = await _repository.GetAsync(c => c.Id == gatewayProviderId);
-
-            if (gatewayProvider == null)
-            {
-                throw new InvalidOperationException("درگاه پرداخت شناسایی نشد.");
-            }
-
-            paymentGatewayProvider = _factory.Create(gatewayProvider.ProviderType, gatewayProvider.Configurations);
-
-            if (paymentGatewayProvider == null)
-            {
-                throw new InvalidOperationException("درگاه پرداخت ساخته نشد.");
-            }
-
-            ExtractCallBackDataResult = paymentGatewayProvider.ExtractCallBackData(command.CallBackData);
-        }
-        else
-        {
-            var gatewayProvider = await _repository.GetAsync(c => c.Id == gatewayProviderId);
-
-            if (gatewayProvider == null)
-            {
-                throw new InvalidOperationException("درگاه پرداخت شناسایی نشد.");
-            }
-
-            paymentGatewayProvider = _factory.Create(gatewayProvider.ProviderType, gatewayProvider.Configurations);
-
-            if (paymentGatewayProvider == null)
-            {
-                throw new InvalidOperationException("درگاه پرداخت ساخته نشد.");
-            }
-
-            ExtractCallBackDataResult = paymentGatewayProvider.ExtractCallBackData(command.CallBackData);
-
-            if (ExtractCallBackDataResult.UniqueRequestId.HasValue)
-            {
-                gatewayPaymentId = ExtractCallBackDataResult.UniqueRequestId;
-                receiptRequest = await _receiptRequestRepository.GetByGatewayPaymentIDAsync(gatewayPaymentId.Value);
-            }
-            else if (!string.IsNullOrEmpty(ExtractCallBackDataResult.CreateReference))
-            {
-                receiptRequest = await _receiptRequestRepository
-                    .GetByGatewayPaymentCreateReferenceAsync
-                    (ExtractCallBackDataResult.CreateReference, gatewayProviderId);
-
-                gatewayPaymentId = receiptRequest?.GatewayPayments
-                    .First(c => c.CreateReference == ExtractCallBackDataResult.CreateReference)
-                    .Id;
-            }
-            else
-            {
-                throw new ArgumentException("پرداخت معتبر نیست.");
-            }
-
-
-            if (receiptRequest is null)
-            {
-                throw new ArgumentException("پرداخت معتبر نیست.");
-            }
-
-            gatewayPayment = receiptRequest.GetGatewayPayment(gatewayPaymentId!.Value);
-
-            if (gatewayPayment is null)
-            {
-                throw new ArgumentException("پرداخت معتبر نیست.");
-            }
-        }
-
-        var result = await receiptRequest.StartCallBackForGatewayPayment(gatewayPayment, paymentGatewayProvider, _clock, ExtractCallBackDataResult);
+        var result = await _callBackGatewayPaymentDomainService.ProcessCallBackAsync(
+            gatewayPaymentId,
+            gatewayProviderId,
+            command.CallBackData);
 
         return new CallBackGatewayPaymentCommandResult
         {
-            ReceiptRequestId = receiptRequest.Id.ToString(CultureInfo.InvariantCulture),
-            Status = receiptRequest.Status,
-            IssuerReference = receiptRequest.IssuerReference,
-            PartyReference = receiptRequest.PartyReference,
-            Amount = receiptRequest.Amount,
-            Currency = receiptRequest.Currency,
-            AdditionalData = receiptRequest.AdditionalData,
+            ReceiptRequestId = result.ReceiptRequest.Id.ToString(CultureInfo.InvariantCulture),
+            Status = result.ReceiptRequest.Status,
+            IssuerReference = result.ReceiptRequest.IssuerReference,
+            PartyReference = result.ReceiptRequest.PartyReference,
+            Amount = result.ReceiptRequest.Amount,
+            Currency = result.ReceiptRequest.Currency,
+            AdditionalData = result.ReceiptRequest.AdditionalData,
             GatewayPayments = new List<CallBackGatewayPaymentGatewayPaymentsCommandResult>
-            {
-                new CallBackGatewayPaymentGatewayPaymentsCommandResult
-                {
-                   Id= gatewayPayment.Id.ToString(CultureInfo.InvariantCulture),
-                   Amount= gatewayPayment.Amount,
-                   Currency= gatewayPayment.Currency,
-                   Status= gatewayPayment.Status,
-                   StatusDescription= gatewayPayment.StatusDescription,
-                   FailedReason= gatewayPayment.FailedReason,
-                }
-            },
+                    {
+                        new CallBackGatewayPaymentGatewayPaymentsCommandResult
+                        {
+                            Id = result.GatewayPayment.Id.ToString(CultureInfo.InvariantCulture),
+                            Amount = result.GatewayPayment.Amount,
+                            Currency = result.GatewayPayment.Currency,
+                            Status = result.GatewayPayment.Status,
+                            StatusDescription = result.GatewayPayment.StatusDescription,
+                            FailedReason = result.GatewayPayment.FailedReason,
+                        }
+                    },
             Issuer = new
             {
-                //todo added issuer fields:
-                // CallbackUrl
-                // ShowPaymentResultPage
+                // اضافه کردن اطلاعات issuer در اینجا
+                // CallbackUrl = result.ReceiptRequest.Issuer.CallbackUrl,
+                // ShowPaymentResultPage = result.ReceiptRequest.Issuer.ShowPaymentResultPage
             }
         };
     }
---
+++ src/PayMaster/Domains/ReceiptRequests/Services/ICallbackGatewayPaymentDomainService.cs
@@ -0,0 +1,26 @@
+﻿using Honamic.PayMaster.PaymentProviders.Models;
+
+namespace Honamic.PayMaster.Domains.ReceiptRequests.Services;
+
+// اینترفیس دامین سرویس
+public interface ICallbackGatewayPaymentDomainService
+{
+    Task<CallBackResult> ProcessCallBackAsync(long? gatewayPaymentId, long gatewayProviderId, string callBackData);
+}
+
+public class CallBackResult
+{
+    public ReceiptRequest ReceiptRequest { get; }
+    public ReceiptRequestGatewayPayment GatewayPayment { get; }
+    public VerifyResult? VerifyResult { get; }
+
+    public CallBackResult(
+        ReceiptRequest receiptRequest,
+        ReceiptRequestGatewayPayment gatewayPayment,
+        VerifyResult? verifyResult)
+    {
+        ReceiptRequest = receiptRequest;
+        GatewayPayment = gatewayPayment;
+        VerifyResult = verifyResult;
+    }
+}
