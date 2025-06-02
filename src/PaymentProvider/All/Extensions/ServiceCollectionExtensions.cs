2208a37a2c5c754800007330a34ebfb2c9f7d63b
Iman Mohammadi <mohammadi4net@gmail.com>
2025-06-02 12:04:11 +0330

    versioning for gateway providers

---
+++ src/PaymentProvider/All/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Honamic.PayMaster.PaymentProvider.PayPal.Extensions;
+using Honamic.PayMaster.PaymentProvider.ZarinPal.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Honamic.PayMaster.PaymentProvider.Behpardakht.Extensions;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAllPaymentProviderServices(this IServiceCollection services)
+    {
+        services.AddZarinPalPaymentProviderServices();
+        services.AddPayPalPaymentProviderServices();
+
+        return services;
+    }
+}
