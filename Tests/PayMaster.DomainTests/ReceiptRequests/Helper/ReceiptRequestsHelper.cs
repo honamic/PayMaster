0b22c7b13bf240d4d483be026555e10bf2cd67a8
Iman Mohammadi <mohammadi4net@gmail.com>
2025-06-07 15:55:35 +0330

    Add tests for CallbackGatewayPaymentDomainService

---
+++ Tests/PayMaster.DomainTests/ReceiptRequests/Helper/ReceiptRequestsHelper.cs
@@ -0,0 +1,44 @@
+﻿using Honamic.Framework.Domain;
+using Honamic.PayMaster.Domains.ReceiptRequests;
+using Honamic.PayMaster.Domains.ReceiptRequests.Parameters;
+using Honamic.PayMaster.Enums;
+
+namespace Honamic.PayMaster.DomainTests.ReceiptRequests.Helper;
+public static class ReceiptRequestsHelper
+{
+    public static ReceiptRequest CreateSampleReceiptRequest(ReceiptRequestStatus status, IIdGenerator _idGenerator)
+    {
+        var receiptRequest = ReceiptRequest.Create(new CreateReceiptRequestParameters
+        {
+            Amount = 10000,
+            Currency = "IRR",
+            Description = "Test payment",
+            DefaultGatewayProviderCode = "sandbox",
+            DefaultIssuerCode = "default",
+            IssuerCode = "",
+            SupportedCurrencies = ["IRR", "USD"],
+            Mobile = "09123456789",
+            Email = "test@example.com",
+            Issuer = new ReceiptRequestIssuerParameters
+            {
+                Id = 1, // Use the ID of the created issuer
+                Enabled = true
+            },
+            GatewayProvider = new ReceiptRequestGatewayProviderParameters
+            {
+                Enabled = true,
+                Id = 789
+            }
+        }, idGenerator: _idGenerator);
+
+        if (status == ReceiptRequestStatus.Doing)
+        {
+            typeof(ReceiptRequest)
+                 .GetProperty(nameof(ReceiptRequest.Status))
+                 ?.SetValue(receiptRequest, status);
+        }
+
+        return receiptRequest;
+    }
+
+}
