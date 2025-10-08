b0cec5013bad45ffbc5dc0baf993fb66926df65f
Iman Mohammadi <mohammadi4net@gmail.com>
2025-10-08 16:44:15 +0330

    refactor pages

---
+++ src/WebSample/Pages/Charge/tracking.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebSample.Pages.Charge
+{
+    public class TrackingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+
+        [BindProperty]
+        public string? ReceiptRequestId { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (string.IsNullOrWhiteSpace(ReceiptRequestId))
+            {
+                ModelState.AddModelError("ReceiptRequestId", "شناسه قبض الزامی است.");
+                return Page();
+            }
+
+            return RedirectToPage("Result", new { ReceiptRequestId });
+        }
+
+    }
+}
